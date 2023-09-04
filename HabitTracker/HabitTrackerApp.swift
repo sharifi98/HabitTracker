94c70d9ba0ea880ed16f967090aae4d5546daa25
Hossein Sharifi <47387238+sharifi98@users.noreply.github.com>
2023-09-04 14:56:33 +0200

    Initial Commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Hossein Sharifi on 04/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
